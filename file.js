117f380173368039841415c8c26ef1104e9b231c
Hakem Allouzi <hakemallouzi@Hakems-MacBook-Pro.local>
2021-05-17 21:23:24 +0300

    var task

---
+++ file.js
@@ -0,0 +1,4 @@
+let name = "Hakem" ;
+let age = 24 ;
+let hobby = "coding" ;
+console.log(`Mr. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`) ;
